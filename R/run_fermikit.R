5cccefca553067fafb44c1cc5c76f944b7c9685a
Jinliang Yang <yangjl0930@gmail.com>
2016-03-29 18:02:21 -0700

    fermikit joint caller

--- R/run_fermikit.R
+++ R/run_fermikit.R
@@ -8,6 +8,7 @@
 #' @param fq An input data.frame for fastq files. Must contains fq1, fq2 and out.
 #' @param kitpath The absolute or relative path of the fermi.kit directory that can invoke the pipeline.
 #' @param ref.fa The full path of genome with bwa indexed reference fasta file.
+#' @param bamdir Path of the bam files.
 #' @param s Approximate genome size, default=2.3g.
 #' @param l Primary read length, default=100.
 #' @param email Your email address that farm will email to once the job was done/failed.
@@ -21,8 +22,12 @@
 #' @examples
 #' fq <- data.frame(fq1=c("f_1.fq", "t_1.fq"), fq2=c("f_1.fq", "t_2.fq"), out=c("t1", "t2"))
 #' run_fermikit(fq, kitpath="/home/jolyang/bin/fermikit/",
-#' genome="/home/jolyang/dbcenter/AGP/AGPv2", s='3g', t=16, l=100, arrayjobs="1-2",
-#' jobid="fermi", email=NULL)
+#'              ref.fa="path/to/fastq.fa", s='2.3g', l=100,
+#'              email=NULL, runinfo = c(FALSE, "bigmemh", 1)
+#'
+#' run_fermikit(bamdir="", kitpath="$HOME/bin/fermikit",
+#'              ref.fa="",
+#'              email=NULL, runinfo = c(FALSE, "bigmemh", 1))
 #'
 #' @export
 run_fermikit <- function(
@@ -43,13 +48,17 @@ run_fermikit <- function(
     #"fermi.kit/seqtk mergepe r1.fq r2.fq | fermi.kit/trimadap-mt -p4" > prefix.mak
     mak <- paste0(fq$out[i], ".mak")
     mag.gz <- paste0(fq$out[i], ".mag.gz")
+    gz <- paste0(fq$out[i], "*.gz")
+    fmd <- paste0(fq$out[i], ".flt.fmd")
     cmd1 <- paste0(kitpath, "/fermi2.pl unitig -s", s, " -t", runinfo[3], " -l", l, " -p ", fq$out[i], " \\", "\n",
                   "\"", kitpath,"/seqtk mergepe ", fq$fq1[i], " ", fq$fq2[i], " | ", " \\\n",
                   kitpath, "/trimadap-mt -p", runinfo[3], "\" > ", mak)
 
     cmd2 <- paste0("make -f ", mak)
     cmd3 <- paste0(kitpath,"/run-calling -t", runinfo[3], " ", ref.fa, " ", mag.gz, " | sh")
-    cat(c(cmd1, cmd2, cmd3), file=shid, sep="\n", append=FALSE)
+    cmd4 <- paste0("rm ", gz)
+    cmd5 <- paste0("rm ", fmd)
+    cat(c(cmd1, cmd2, cmd3, cmd4, cmd5), file=shid, sep="\n", append=FALSE)
   }
 
   message(sprintf("###>>> mergepe, trimadap-mt and then fermi unitig !"))
@@ -61,6 +70,33 @@ run_fermikit <- function(
 }
 
 #' @rdname run_fermikit
-run_fermikit_vcfcall <- function(){
+#' @examples
+
+#'
+#' @export
+run_fermikit_vcfcall <- function(
+  bamdir="",
+  kitpath="$HOME/bin/fermikit",
+  ref.fa="",
+  email=NULL, runinfo = c(FALSE, "bigmemh", 1)
+){
+
+  # create dir if not exist
+  dir.create("slurm-script", showWarnings = FALSE)
+  #It is also possible to call SNPs and short INDELs
+  #from multiple BAMs at the same time and produce a multi-sample VCF:
+  #fermi.kit/htsbox pileup -cuf ref.fa pre1.srt.bam pre2.srt.bam > out.raw.vcf
+  #fermi.kit/k8 fermi.kit/hapdip.js vcfsum -f out.raw.vcf > out.flt.vcf
+
+  cmd1 <- paste0("cd ", bamdir)
+  cmd2 <- paste0(kitpath, "/htsbox pileup -cuf ", ref.fa, " *srt.bam", " > out.raw.vcf")
+  cmd3 <- paste0(kitpath,"/k8 ", kitpath,"/hapdip.js vcfsum -f out.raw.vcf > out.flt.vcf")
+
+  message(sprintf("###>>> call SNPs and short INDELs from multiple BAMs !"))
+  shcode <- c(cmd1, cmd2, cmd3)
+  set_array_job(shid="slurm-script/run_fermikit_array.sh",
+                shcode=shcode, arrayjobs="1",
+                wd=NULL, jobid="fermikit", email=email, runinfo=runinfo)
+  #  sbatch -p bigmemh --mem 32784 --ntasks=4  slurm-script/run_gatk_array.sh
 
 }
