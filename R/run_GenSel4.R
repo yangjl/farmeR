d361567a6bd9736e6501b89f1fbbee74d2055a44
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-04-09 21:02:39 -0700

    a new verion of run_GelSel

--- R/run_GenSel4.R
+++ R/run_GenSel4.R
@@ -8,6 +8,7 @@
 #'
 #' @param inputdf An input data.frame.
 #' @param email Your email address that farm will email to once the jobs were done/failed.
+#' @param cmdno Number of commands per CPU, i.e. number of rows per inputdf.
 #' @param runinfo Parameters specify the array job partition information.
 #' A vector of c(FALSE, "bigmemh", "1"): 1) run or not, default=FALSE
 #' 2) -p partition name, default=bigmemh and 3) --cpus, default=1.
@@ -21,13 +22,14 @@
 #'    geno="/Users/yangjl/Documents/GWAS2_KRN/SNP/merged/geno_chr.newbin",
 #'    trainpheno="/Users/yangjl/Documents/Heterosis_GWAS/pheno2011/reports/cd_GenSel_fullset.txt",
 #'    testpheno="/Users/yangjl/Documents/Heterosis_GWAS/pheno2011/reports/cd_GenSel_fullset.txt",
-#'    chainLength=1000, burnin=100, varGenotypic=1.4, varResidual=2)
+#'    chainLength=1000, burnin=100, varGenotypic=1.4, varResidual=2,
+#'    out="test_out")
 #'
 #' run_GenSel4(inputdf, inpdir="slurm-script", email=NULL, runinfo = c(FALSE, "bigmemh", 1) )
 #'
 #' @export
 run_GenSel4 <- function(
-  inputdf, inpdir="largedata/",
+  inputdf, inpdir="largedata/", cmdno=1,
   email=NULL, runinfo = c(FALSE, "bigmemh", 1)
 ){
 
@@ -37,20 +39,22 @@ run_GenSel4 <- function(
   dir.create(inpdir, showWarnings = FALSE)
 
   for(i in 1:nrow(inputdf)){
-    shid <- paste0(inpdir, "/run_gensel_", i, ".inp")
+    inpid <- paste0(inpdir, "/", inputdf$out[i], ".inp")
     ### output the inp file:
     GS_cv_inp(
-      inp= shid, pi=inputdf$pi[i], geno=inputdf$geno[i],
+      inp= inpid, pi=inputdf$pi[i], geno=inputdf$geno[i],
       trainpheno=inputdf$trainpheno[i], testpheno=inputdf$testpheno[i],
       chainLength=inputdf$chainLength[i], burnin=inputdf$burnin[i],
       varGenotypic=inputdf$varGenotypic[i], varResidual=inputdf$varResidual[i]
     )
   }
 
-  shcode <- paste0("GenSel4R ", inpdir, "/run_gensel", "_$SLURM_ARRAY_TASK_ID.inp > ",
-                   inpdir, "/run_gensel", "_$SLURM_ARRAY_TASK_ID.log")
+  ### setup shell id
+  set_GS(inputdf, cmdno, inpdir)
+
+  shcode <- paste0("sh slurm-script/run_gensel_$SLURM_ARRAY_TASK_ID.sh")
   set_array_job(shid="slurm-script/run_gensel_array.sh",
-                shcode=shcode, arrayjobs=paste("1", nrow(inputdf), sep="-"),
+                shcode=shcode, arrayjobs=paste("1", nrow(inputdf)/cmdno, sep="-"),
                 wd=NULL, jobid="gensel", email=email, runinfo=runinfo)
 }
 
@@ -97,3 +101,23 @@ GS_cv_inp <- function(
   )
 }
 
+set_GS <- function(inputdf, cmdno, inpdir){
+  ####
+  for(j in 1:(nrow(inputdf)/cmdno)){
+
+    srow <- cmdno*(j-1) + 1
+    erow <- cmdno*j
+    shid <- paste0("slurm-script/run_gensel_", j, ".sh")
+
+    sh1 <- paste0("GenSel4R ", inpdir, "/", inputdf$out[srow:erow], ".inp > ",
+                     inpdir, "/", inputdf$out[srow:erow], ".log")
+    sh2 <- c(paste0("rm ", inpdir, "/", inputdf$out[srow:erow], ".mcmcSamples1"),
+             paste0("rm ", inpdir, "/", inputdf$out[srow:erow], ".mrkRes1"),
+             paste0("rm ", inpdir, "/", inputdf$out[srow:erow], ".inp"),
+             paste0("rm ", inpdir, "/", inputdf$out[srow:erow], ".out1"),
+             paste0("rm ", inpdir, "/", inputdf$out[srow:erow], ".cgrRes1"))
+    cat(paste("### run GenSel", Sys.time(), sep=" "),
+        c(sh1, sh2),
+        file=shid, sep="\n", append=FALSE)
+  }
+}
