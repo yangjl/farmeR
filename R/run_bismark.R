db2473c93b0f5603f0d73eebbc21f2ef309cb4c8
Jinliang Yang <yangjl0930@gmail.com>
2016-06-24 14:43:27 -0700

    add more functions

--- R/run_bismark.R
+++ R/run_bismark.R
@@ -9,12 +9,22 @@
 #' see more detail about Bismark:
 #' \url{http://www.bioinformatics.babraham.ac.uk/projects/bismark/Bismark_User_Guide.pdf}
 #'
-#' @param input_df An input data.frame object. Must contains fq1, fq2 and out.
+#' (I) Running bismark_genome_preparation
+#' module load bismark/0.14.3
+#' module load bowtie2/2.2.5
+#' bismark_genome_preparation --bowtie2 /home/jolyang/dbcenter/AGP/AGPv2/
+#'
+#' (II) Running bismark
+#'
+#' #uses 0-based genomic start and 1-based end coordinates.
+#' bismark_methylation_extractor -s --bedGraph --counts --buffer_size 10G --CX
+#' --cytosine_report --genome_folder /home/jolyang/dbcenter/AGP/AGPv2 test_pe.bam
+#' #<chromosome> <position> <strand> <count methylated> <count unmethylated> <C-context> <trinucleotide context>
+#'
+#' @param inputdf An input data.frame object. Must contains fq1, fq2 and out, bam (optional).
 #' @param genome The folder of genome prepared by bismark.
-#' @param cpu Number of CPU to use per job.
+#' @param N Number of miss match.
 #' @param outdir Folder for output.
-#' @param arrayjobs A character specify the number of array you try to run, i.e. 1-100.
-#' @param jobid The job name show up in your sq NAME column.
 #' @param email Your email address that farm will email to once the job was done/failed.
 #'
 #' @return return a batch of shell scripts.
@@ -25,35 +35,42 @@
 #' cpu=4, outdir="/group/jrigrp4/BS_teo20/WGBS/BSM", arrayjobs="1-5", jobid="bs1-5",
 #' email=NULL)
 #' @export
-runa_bismark <- function(input_df,
-                         genome="/home/jolyang/dbcenter/AGP/AGPv2",
-                         cpu=4,
-                         outdir="/group/jrigrp4/BS_teo20/WGBS/BSM",
-                         arrayjobs="1-5",
-                         jobid="bs1-5",
-                         email="yangjl0930@gmail.com"){
+run_bismark <- function(inputdf,
+                        genome="/home/jolyang/dbcenter/AGP/AGPv2",
+                        outdir="/group/jrigrp4/BS_teo20/WGBS/BSM",
+                        N=1,
+                        align=TRUE, email=NULL, runinfo = c(FALSE, "bigmemh", 1)){
 
   # create dir if not exist
   dir.create("slurm-script", showWarnings = FALSE)
+  ### determine memory based on partition
+  runinfo <- get_runinfo(runinfo)
 
 
-  for(i in 1:nrow(input_df)){
+  for(i in 1:nrow(inputdf)){
 
     shid <- paste0("slurm-script/run_bismark_", i, ".sh")
-    #out <- gsub(".*/", "", out)
-    #outfile <- paste0(outdir, "/", out)
-    cmd <- paste("bismark --bowtie2 -n 1", genome, "-p", cpu,
-                 "-1", input_df$fq1[i],  "-2", input_df$fq2[i],
-                 "--output_dir", outdir,  "--basename", input_df$out[i])
+    cmd1 <- paste("bismark --bowtie2 -N", N, genome, "-p", runinfo[3],
+                  "-1", inputdf$fq1[i],  "-2", inputdf$fq2[i],
+                  "--output_dir", outdir,  "--basename", inputdf$out[i])
+    cmd2 <- paste("bismark_methylation_extractor -p --bedGraph --counts --buffer_size 50%",
+                  "-o", outdir,
+                  "--CX --cytosine_report --genome_folder", genome,
+                  paste0(inputdf$out[i], "_pe.bam"))
 
+    if(align){
+      cmd <- c(cmd1, cmd2)
+    }else{
+      cmd <- cmd2
+    }
     cat(cmd, file=shid, sep="\n", append=FALSE)
   }
 
-  shcode <- paste("module load bismark/0.14.3", "module load bowtie2/2.2.5",
+  shcode <- paste("module load bismark/0.14.3",
+                  "module load bowtie2/2.2.5",
                   "sh slurm-script/run_bismark_$SLURM_ARRAY_TASK_ID.sh", sep="\n")
-
   set_array_job(shid="slurm-script/run_bismark_array.sh",
-                shcode=shcode, arrayjobs=arrayjobs,
-                wd=NULL, jobid=jobid, email=email)
+                shcode=shcode, arrayjobs=paste("1", nrow(inputdf), sep="-"),
+                wd=NULL, jobid="bismark", email=email, runinfo=runinfo)
 }
 
